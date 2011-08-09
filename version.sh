62387c11f3532d5753f000c41663446f02e3afd9
Reinhard Tartler <siretart@tauware.de>
2011-08-09 20:43:07 +0200

    Imported Upstream version 0.116.2042+git178455c

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Script modified from upstream source for Debian packaging since packaging
 # won't include .git repository.
-echo '#define X264_VERSION " r2037 f8ebd4a"'
-echo '#define X264_POINTVER "0.116.2037 f8ebd4a"'
+echo '#define X264_VERSION " r2042 178455c"'
+echo '#define X264_POINTVER "0.116.2042 178455c"'
