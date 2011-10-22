472f39c76b7a3a68a680d214c7517d9c6e5a5a30
Rico Tzschichholz <ricotz@t-online.de>
2011-10-22 23:19:58 +0200

    Imported Upstream version 0.118.2092+git6eac7c3

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Script modified from upstream source for Debian packaging since packaging
 # won't include .git repository.
-echo '#define X264_VERSION " r2085 8a62835"'
-echo '#define X264_POINTVER "0.118.2085 8a62835"'
+echo '#define X264_VERSION " r2092 6eac7c3"'
+echo '#define X264_POINTVER "0.118.2092 6eac7c3"'
