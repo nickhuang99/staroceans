20038120c998edb4e8b9f63028804556ca5af5d5
Reinhard Tartler <siretart@tauware.de>
2011-12-31 09:05:59 +0100

    Imported Upstream version 0.119.2113+gitcc129ad

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Script modified from upstream source for Debian packaging since packaging
 # won't include .git repository.
-echo '#define X264_VERSION " r2092 6eac7c3"'
-echo '#define X264_POINTVER "0.118.2092 6eac7c3"'
+echo '#define X264_VERSION " r2113 cc129ad"'
+echo '#define X264_POINTVER "0.119.2113 cc129ad"'
