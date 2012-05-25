e09bd4355f87e533d104e341c7e6fb8af37f2b10
Rico Tzschichholz <ricotz@t-online.de>
2012-05-25 07:32:56 +0200

    Imported Upstream version 0.124.2198+git1c97f35

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Script modified from upstream source for Debian packaging since packaging
 # won't include .git repository.
-echo '#define X264_VERSION " r2189 35cf912"'
-echo '#define X264_POINTVER "0.123.2189 35cf912"'
+echo '#define X264_VERSION " r2198 1c97f35"'
+echo '#define X264_POINTVER "0.124.2198 1c97f35"'
